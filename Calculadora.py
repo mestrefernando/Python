0e473a1c59654237a1a64bb8f94203a2883e9c39
Fernando Mestre <67239359+mestrefernando@users.noreply.github.com>
2020-10-17 00:17:12 +0100

    Update Calculadora.py
    Code that receives value entered by the user, logical evaluation by the machine through the user (VS Code).

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+Em Português
 Calculadora
 Autor: Fernando Mestre
 Soma, Subtracção, Divisão, Multiplicação e Expoente
@@ -26,3 +27,32 @@ while sair == False:
     teste = input("Deseja sair (Sim/Não?): ")
     if teste == "s":
         sair = True
+
+"""In english""" 
+
+Calculator
+Author: Fernando Mestre
+Sum, Subtraction, Division, Multiplication and Exponent
+"""
+print("-----Basic Calculator-----")
+
+exit = False
+while out == False:
+    num1 = float(input("Type first number: "))
+    operator = (input("Type operator: "))
+    num2 = float(input("Type second number: "))
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "/":
+        print(num1 / num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "**":
+        print(num1 ** num2)
+    else:
+        print("Invalid operator")
+    test = input("Do you want to leave (Yes/No?): ")
+    if test == "s":
+        exit = True
