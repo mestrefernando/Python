722c4532bda82746dea65fdc01acdb52c62ac679
Fernando Mestre <67239359+mestrefernando@users.noreply.github.com>
2020-11-27 11:01:53 +0000

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,44 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-(VS Code)
-Em Português
-Calculadora
-Autor: Fernando Mestre
-Soma, Subtracção, Divisão, Multiplicação e Expoente
-"""
-print("-----Calculadora básica-----")
-
-sair = False
-while sair == False:
-    num1 = float(input("Digite o primeiro número: "))
-    operador = (input("Digite o operador: "))
-    num2 = float(input("Digite o segundo número: "))
-    if operador == "+":
-        print(num1 + num2)
-    elif operador == "-":
-        print(num1 - num2)
-    elif operador == "/":
-        print(num1 / num2)
-    elif operador == "*":
-        print(num1 * num2)
-    elif operador == "**":
-        print(num1 ** num2)
-    else:
-        print("Operador inválido")
-    teste = input("Deseja sair (Sim/Não?): ")
-    if teste == "s":
-        sair = True
-
-"""In english""" 
-
 Calculator
 Author: Fernando Mestre
-Sum, Subtraction, Division, Multiplication and Exponent
+Sum, Subtraction, Division, Multiplication, Exponent, Modulo and Integer Division
 """
-print("-----Basic Calculator-----")
-
+print ("-----Basic Calculator-----")
 exit = False
-while out == False:
+while exit == False:
     num1 = float(input("Type first number: "))
     operator = (input("Type operator: "))
     num2 = float(input("Type second number: "))
@@ -52,8 +20,13 @@ while out == False:
         print(num1 * num2)
     elif operator == "**":
         print(num1 ** num2)
+    elif operator == "//":
+        print(num1 // num2)
+    elif operator == "%":
+        print(num1 % num2)
     else:
         print("Invalid operator")
     test = input("Do you want to leave (Yes/No?): ")
-    if test == "s":
+    if test == "Yes":
         exit = True
+
